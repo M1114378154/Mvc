bc5f84d350c249e92e192d2536c37d674972439a
M1114378154 <yangtianmin44@qq.com>
2019-01-10 15:26:16 +0800

    0110

---
+++ MvcApplication1/WebApplication1/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Artist  //添加艺术家模型类
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}

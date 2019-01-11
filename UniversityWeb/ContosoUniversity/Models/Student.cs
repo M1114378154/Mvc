9f411a95ac04a0b13052e97fe3a601b7a0d2189d
M1114378154 <yangtianmin44@qq.com>
2019-01-11 13:20:58 +0800

    0111

---
+++ UniversityWeb/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+        [Display(Name="姓名")] //数据注解
+
+        public string Name { get; set; }
+        [Display(Name = "注册日期")]
+        public DateTime EnrollmentDate { get; set; }  //注册事件
+
+        public string Image { get; set; }
+        [Display(Name="选课信息")]
+
+        //导航属性 来表示（一对多关系）   Enrollment注册
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
